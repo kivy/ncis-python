9719308d18288d857207b8e5aa34c231a379780e
Mathieu Virbel <mat@kivy.org>
2019-01-30 20:21:10 +0100

    add inspect

--- ncis_python/__init__.py
+++ ncis_python/__init__.py
@@ -1,6 +1,7 @@
-from ncis import route, api_response, request
+from ncis import route, api_response, request, ncis_weakrefs
 import sys
 import platform
+import inspect
 
 __version__ = "0.1"
 __author__ = "Mathieu Virbel <mat@kivy.org>"
@@ -48,8 +49,20 @@ def _eval():
     return api_response(result)
 
 
-@route("/inspect", method="POST")
-def _inspect():
-    cmd = request.forms.get("cmd")
-    result = eval(cmd, globals(), globals())
-    return api_response(result)
+@route("/inspect/<refid>", method="GET")
+def _inspect(refid):
+    refid = int(refid)
+    try:
+        obj = ncis_weakrefs.get(refid)
+        if obj is None:
+            return api_response(None)
+        obj = obj()
+        if obj is None:
+            ncis_weakrefs.pop(refid, None)
+            return api_response(None)
+        result = inspect.getmembers(obj)
+        return api_response(result)
+    except Exception as e:
+        import traceback; traceback.print_exc()
+        ncis_weakrefs.pop(refid, None)
+        return api_response(None)
